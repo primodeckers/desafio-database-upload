977af6f18b9fde0f8ca2aea1d60d8f0577a554fc
Renê Estevam Deckers <primodeckers@hotmail.com>
2021-03-01 10:27:51 -0300

    Fundamentos nodejs

---
+++ src/models/Transaction.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  JoinColumn,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
+class Transaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  type: 'income' | 'outcome';
+
+  @Column()
+  value: number;
+
+  @ManyToOne(() => Category, category => category.transaction, { eager: true })
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @Column()
+  category_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
+
+export default Transaction;
